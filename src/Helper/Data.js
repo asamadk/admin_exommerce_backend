7dd2eb630550a737c14062ff1c48c5b103b64943
Abdul Samad <abdul.samadkirmani.samad63@gmail.com>
2022-09-05 18:27:11 +0530

    Product modal

--- src/Helper/Data.js
+++ src/Helper/Data.js
@@ -1,3 +1,7 @@
 export const NAVIGATION_DATA = [
     'Dashboard','User','Orders', 'Coupon', 'Category', 'Product', 'Add','Login'
 ]
+
+export const COLORS = [
+    '#673ab7', '#03a9f4', '#ffc107', '#009688', '#f44336', '#8bc34a', '#b23c17'
+]
--- src/Pages/Coupon.js
+++ src/Pages/Coupon.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import LockIcon from "@mui/icons-material/Lock";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import LoadingButton from "@mui/lab/LoadingButton";
+import Switch from "@mui/material/Switch";
+import Paper from "@mui/material/Paper";
 
 export default function Coupon() {
   return (
-    <div>Coupon</div>
+    <>
+      coupon
+    </>
   )
 }
